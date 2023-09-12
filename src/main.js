559a11b4280bf46de3b2d1cb139556b5cbf6215a
mamadjavaad <mamadjavad8237@gmail.com>
2023-09-12 20:41:33 +0330

    adding vuetify ✔️

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
 import './assets/base.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
 const app = createApp(App)
 axios.defaults.baseURL = "https://api.github.com/"
 app.config.globalProperties.$http = axios
 app.use(router)
-
+app.use(vuetify)
 app.mount('#app')
