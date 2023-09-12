95fec3b08d867fb678dd1f2673936dd98df8c919
mamadjavaad <mamadjavad8237@gmail.com>
2023-09-12 20:41:36 +0330

    setting the home view 🏗️

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup>
+// import { ref} from 'vue'
 
 </script>
 
 <template>
-  <main>
-    <h2>Home</h2>
-  </main>
+
+    <v-main class="d-flex align-center justify-center flex-column">
+      <div class="search-box">
+        <v-text-field
+        bg-color="#ffffff30"
+        density="compact"
+        variant="solo"
+        label="Search github cards"
+        append-inner-icon="mdi-magnify"
+        single-line
+        hide-details
+        @click:append-inner="onClick"
+      ></v-text-field>
+      </div>
+
+  
+    </v-main>
 </template>
+
+<style lang="scss" scoped>
+.v-main {
+  min-height: 100vh;
+}
+.search-box {
+  width: 35%;
+ .v-text-field{
+  backdrop-filter: blur(20px);
+ }
+}
+
+</style>
