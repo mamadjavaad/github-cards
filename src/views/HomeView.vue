01d81653064ea51c3b38593a6466ca7180dc2698
mamadjavaad <mamadjavad8237@gmail.com>
2023-09-12 20:41:37 +0330

    updateing home view🚀

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,68 @@
 <script setup>
-// import { ref} from 'vue'
+import { ref } from 'vue'
+let defCards = [
+  {
+    prependIcon: 'mdi-clock-outline',
+    title: 'mamadjavaad',
+  },
+  {
+    prependIcon: 'mdi-vuejs',
+    title: 'vuejs',
+  },
+  {
+    prependIcon: 'mdi-vuetify',
+    title: 'vuetifyjs',
+  },
+]
+let searchModel = ref(null)
+let searchSubmit = () => {
+  alert(searchModel.value)
+}
 
 </script>
 
 <template>
+  <v-main class="d-flex align-center justify-center flex-column">
+    <v-card class="pa-8 d-flex justify-center flex-wrap">
+      <v-responsive max-width="550">
+        <v-img class="mx-auto mb-11 p-2 search-img" max-height="140" max-width="300"
+          src="src/assets/img/githubSearch.png"></v-img>
 
-    <v-main class="d-flex align-center justify-center flex-column">
-      <div class="search-box">
-        <v-text-field
-        bg-color="#ffffff30"
-        density="compact"
-        variant="solo"
-        label="Search github cards"
-        append-inner-icon="mdi-magnify"
-        single-line
-        hide-details
-        @click:append-inner="onClick"
-      ></v-text-field>
-      </div>
+        <div class="search-box">
+          <v-text-field density="compact"  variant="solo"  theme="light" bg-color="#9d9d9d52"
+            label="Search github cards" append-inner-icon="mdi-github" single-line hide-details v-model="searchModel" @keydown.enter="searchSubmit"
+            @click:append-inner="searchSubmit"></v-text-field>
+        </div>
 
-  
-    </v-main>
+      </v-responsive>
+    </v-card>
+
+  </v-main>
 </template>
 
 <style lang="scss" scoped>
 .v-main {
+  backdrop-filter: blur(12px);
   min-height: 100vh;
 }
-.search-box {
-  width: 35%;
- .v-text-field{
-  backdrop-filter: blur(20px);
- }
+
+.v-card {
+  width: 100%;
+  box-shadow: none;
+  background: rgba(0, 128, 128, 0);
 }
 
+.v-text-field {
+  backdrop-filter: blur(20px);
+}
+
+.v-field {
+  color: #a0a0a0 !important;
+}
+
+.search-img {
+  background: #9d9d9d52;
+  border-radius: 11px;
+  backdrop-filter: blur(12px);
+}
 </style>
