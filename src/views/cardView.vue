619bd53ebb69f60769b5fdbb29f48fa7d83ecf2b
mamadjavaad <mamadjavad8237@gmail.com>
2023-09-12 20:41:40 +0330

    adding repos 🚀

--- src/views/cardView.vue
+++ src/views/cardView.vue
@@ -2,10 +2,14 @@
 //imports
 import { ref, onMounted, getCurrentInstance, computed } from 'vue'
 import { useRoute } from 'vue-router'
+// avatar loading data
+const avatarLoad = ref(false)
+const imageToggle = () => {
+  avatarLoad.value = true
+}
 //getting param of the page we are in
 const route = useRoute()
 const username = computed(() => route.params.username)
-
 //using axios as axios.req
 const app = getCurrentInstance()
 const axios = app.appContext.config.globalProperties.$http
@@ -21,18 +25,36 @@ const getUser = async () => {
       alert('error')
     })
 }
+//getting repos data
+const repoData = ref(null)
 
+const getRepo = async () => {
+  await axios.get('users/' + username.value + '/repos')
+    .then((response) => {
+      repoData.value = response.data
+    })
+    .catch((error) => {
+      console.log(error.message)
+    })
+}
+//running get functions on mounted
 onMounted(() => {
   getUser()
+  getRepo()
 })
 </script>
 
 <template>
   <v-main class="d-flex align-center justify-center flex-column">
 
-    <div class="github-box d-flex " v-if="userData && userData.avatar_url">
+    <div class="github-box d-flex " v-if="userData && repoData">
       <div class="sidebar d-flex flex-column pa-3 align-center ">
-        <img :src="userData && userData.avatar_url ? userData.avatar_url : null" alt="profile photo" class="mb-4">
+        <img v-show="avatarLoad" @load="imageToggle" :src="userData && userData.avatar_url ? userData.avatar_url : null"
+          alt="profile photo" class="mb-4">
+        <div class="loading d-flex justify-center align-center rounded-circle" v-if="!avatarLoad">
+          <v-progress-circular indeterminate color="white" :size="50"></v-progress-circular>
+        </div>
+
         <span class="mb-0">{{ username }}</span>
         <span class="mb-3 acc-type">{{ userData && userData.type ? userData.type : null }}</span>
         <div class="d-flex flex-wrap justify-space-evenly mb-4 w-100 follow-data">
@@ -63,24 +85,38 @@ onMounted(() => {
           <span class="text-center details mb-2">
             <v-icon icon="mdi-account"></v-icon>
             Fullname : {{ userData && userData.name ? userData.name : 'N/A' }}</span>
-        <span   v-if="userData.email" class="text-center details mb-2">
-          <v-icon icon="mdi-email" class="mr-2"></v-icon>
-email : {{ userData && userData.email ? userData.email : 'N/A' }}
-
-        </span>
-            <span class="text-center details mb-2" v-if="userData.location">
+          <span v-if="userData.email" class="text-center details mb-2">
+            <v-icon icon="mdi-email" class="mr-2"></v-icon>
+            email : {{ userData && userData.email ? userData.email : 'N/A' }}
+          </span>
+          <span class="text-center details mb-2" v-if="userData.location">
             <v-icon icon="mdi-pin" class="mr-2"></v-icon>
             location : {{ userData && userData.location ? userData.location : 'N/A' }}</span>
-            <span  class="text-center details mb-2" v-if="userData.blog">
+          <span class="text-center details mb-2" v-if="userData.blog">
             <v-icon icon="mdi-post"></v-icon>
-              blog : <a class="text-grey" target="_blank" :href="userData && userData.blog ?userData.blog :'' ">link</a></span>
+            blog : <a class="text-grey" target="_blank"
+              :href="userData && userData.blog ? userData.blog : ''">link</a></span>
         </div>
+      </div>
+      <div class="public-repos d-flex flex-column " v-if="repoData">
+        <h3>Public repos</h3>
+        <div class=" w-100 d-flex flex-wrap justify-space-between repos-container">
+          <div class="repo mt-3 pa-2 d-flex flex-column" v-for="repo in repoData" :key="repo">
+            <a target="_blank" :href="repo.html_url" class="text-blue">{{ repo.name }}</a>
+            <span class="mb-1 subtext" v-if="repo.fork">Forked</span>
+            <span class="mb-1 subtext">{{ repo.description }}</span>
+            <div class="w-100" v-if="repo.language">
+              <v-icon class=" subtext text-black" icon="mdi-checkbox-blank-badge-outline"></v-icon>
+              <span class="subtext pa-2" v-if="repo.language">{{ repo.language }}</span>
+            </div>
+          </div>
 
+        </div>
 
       </div>
     </div>
 
-    <div v-if="!userData || !userData.avatar_url">
+    <div v-if="!userData || !repoData">
       <div class="loading d-flex justify-center align-center">
         <v-progress-circular indeterminate color="white" :size="50"></v-progress-circular>
       </div>
@@ -103,7 +139,7 @@ email : {{ userData && userData.email ? userData.email : 'N/A' }}
 
 .github-box {
   border: 1px solid $boxborderColor;
-  width: 42%;
+  width: 95%;
   border-radius: 5px;
   background-color: $boxbackgroundColor;
   backdrop-filter: blur(20px);
@@ -149,6 +185,59 @@ email : {{ userData && userData.email ? userData.email : 'N/A' }}
     }
   }
 
+  .public-repos {
+    width: 65%;
+    height: inherit;
+    padding: 12px;
 
+    .repos-container {
+      max-height: 450px;
+      overflow-y: auto;
+
+      .repo {
+        width: 49%;
+        // height: 150px;
+        border-radius: 5px;
+        border: 1px solid $boxborderColor;
+        background: #b9babb24;
+
+        .subtext {
+          opacity: 0.6;
+          font-size: 11.5px;
+        }
+
+      }
+    }
+
+
+  }
 }
-</style>
+
+//responsivity
+@media screen and (min-width: 650px) {
+  .github-box {
+    max-width: 920px;
+    min-width: 620px;
+  }
+}
+
+@media screen and (max-width:650px) {
+  .github-box {
+    flex-direction: column !important;
+    background: none;
+    border: 0;
+  }
+
+  .sidebar,
+  .public-repos {
+    width: 100% !important;
+    margin-top: 15px;
+    border: 1px solid $boxborderColor;
+    background: $boxbackgroundColor;
+    border-radius: 5px;
+  }
+
+  .repo {
+    width: 100% !important;
+  }
+}</style>
