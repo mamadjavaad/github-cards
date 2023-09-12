b202c3d3cfc9ec4b446588605aa6edeb3536813c
mamadjavaad <mamadjavad8237@gmail.com>
2023-09-12 20:41:39 +0330

    adding datas to card 🏗️

--- src/views/cardView.vue
+++ src/views/cardView.vue
@@ -1,16 +1,24 @@
-<script setup> 
-import { ref, onMounted, getCurrentInstance } from 'vue'
+<script setup>
+//imports
+import { ref, onMounted, getCurrentInstance, computed } from 'vue'
+import { useRoute } from 'vue-router'
+//getting param of the page we are in
+const route = useRoute()
+const username = computed(() => route.params.username)
+
+//using axios as axios.req
 const app = getCurrentInstance()
 const axios = app.appContext.config.globalProperties.$http
 //getting user data
 const userData = ref(null)
 const getUser = async () => {
-  await axios.get('users/mamadjavaad')
+  await axios.get('users/' + username.value)
     .then((response) => {
       userData.value = response.data
     })
     .catch((error) => {
       console.log(error.message)
+      alert('error')
     })
 }
 
@@ -20,32 +28,93 @@ onMounted(() => {
 </script>
 
 <template>
-    <v-main class="d-flex align-center justify-center flex-column">
+  <v-main class="d-flex align-center justify-center flex-column">
 
-      <div class="github-box d-flex ">
-        <img :src="userData && userData.avatar_url ? userData.avatar_url : null" alt="profile">
+    <div class="github-box d-flex " v-if="userData && userData.avatar_url">
+      <div class="sidebar d-flex flex-column pa-3 align-center ">
+        <img :src="userData && userData.avatar_url ? userData.avatar_url : null" alt="profile photo" class="mb-4">
+        <span class="mb-0">{{ username }}</span>
+        <span class="mb-3 acc-type">{{ userData && userData.type ? userData.type : null }}</span>
+        <div class="d-flex flex-wrap justify-lg-space-evenly mb-4 w-100 follow-data">
+          <v-icon icon="mdi-account-multiple "></v-icon>
+          <span>{{ userData && (userData.followers !== null && userData.followers !== undefined) ?
+            userData.followers : 'N/A' }} Followers</span>
+          <span>{{ userData && (userData.following !== null && userData.following !== undefined) ?
+            userData.following : 'N/A' }} Following</span>
+        </div>
+        <a :href="userData && userData.html_url ? userData.html_url : null" target="_blank">
+          <v-btn prepend-icon="mdi-github" variant="" class="mb-4">
+            Github Page
+          </v-btn>
+        </a>
+        <span class="mb-4 text-center bio" v-if="userData && userData.bio">
+          {{ userData && userData.bio ? userData.bio : null }}
+        </span>
       </div>
-    </v-main>
+    </div>
+
+    <div v-if="!userData || !userData.avatar_url">
+      <div class="loading d-flex justify-center align-center">
+        <v-progress-circular indeterminate color="white" :size="50"></v-progress-circular>
+      </div>
+    </div>
+  </v-main>
 </template>
 
 <style lang="scss" scoped>
 .v-main {
   min-height: 100vh;
 }
+
+.loading {
+  background: #9d9d9d52;
+  border-radius: 11px;
+  backdrop-filter: blur(12px);
+  width: 100px;
+  height: 100px;
+}
+
 .github-box {
   border: 1px solid $boxborderColor;
   width: 42%;
-  height: 550px;
+  min-height: 550px;
   border-radius: 5px;
   background-color: $boxbackgroundColor;
   backdrop-filter: blur(20px);
   color: white;
 
-  img {
-    width: 160px;
-    height: 160px;
-    border-radius: 50%;
-    border: 1px solid $boxborderColor;
+  .sidebar {
+    width: 35%;
+    height: inherit;
+    border-right: 1px solid $boxborderColor;
+
+    img {
+      width: 140px;
+      height: 140px;
+      border-radius: 50%;
+      border: 1px solid $boxborderColor;
+    }
+
+    .acc-type {
+      opacity: 0.6;
+      font-size: 13.3px;
+    }
+
+
+    .follow-data {
+      font-size: 13.3px;
+    }
+
+    .v-btn {
+      border: 1px solid $boxborderColor;
+      background: rgba(185, 186, 187, 0.1019607843);
+    }
+
+    .bio {
+      font-size: 14px;
+    }
   }
+
+
 }
 </style>
